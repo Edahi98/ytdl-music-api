b9e8ae50fe484171b8aba7ea64bc3351f86c8414
Edahi98 <garciaedahi3822@gmail.com>
2025-04-22 17:15:39 -0600

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const { ytmp3 } = require("@hiudyy/ytdl");
 const express = require("express")
 const app = express();
+const cors = require("cors")
 const port = process.env.PORT || 3001;
+app.use(cors())
 
 app.get("/ytdl-audio", (req, res)=>{
     ytmp3(req.query.url).then(audio =>{
